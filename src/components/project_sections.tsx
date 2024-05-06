6a4f7526291915721500eeb9e7ee9a9520f30f5c
Sid <siddharthjoshi1@hotmail.com>
2024-05-06 22:57:55 +0100

    Added basic project section

--- src/components/project_sections.tsx
+++ src/components/project_sections.tsx
@@ -1,4 +1,4 @@
-"use client";
+
 
 import Image from "next/image";
 
@@ -16,16 +16,37 @@ export default function ProjectSection(props: projectProps) {
       <br></br>
 
       <div className=" grid grid-cols-1 lg:grid-cols-5 gap-5  ">
-        {/* proj 1 */}
-        <div className="h-[300px] lg:h-[500px] flex lg:col-span-2 flex-col items-center justify-start bg-rich-black hover:bg-oil ">
+        {props.projects!.map((project, i) => (
+          // check if is wide then add col span-3 else have col-span-2
+
+          <div
+            key={i}
+            className={`${project.isWide!.toString() == "true" ? "lg:col-span-3" : "lg:col-span-2"}`}
+          >
+            <div className=" h-[300px] md:h-[350px] lg:h-[500px] flex flex-col items-center  justify-start bg-rich-black hover:bg-oil">
+              <div className="w-full">
+                <div
+                  className={`h-[120px] md:h-[200px] lg:h-[300px] bg-[url('/${project.image_url}')] bg-no-repeat bg-cover`}
+                ></div>
+                <p className="px-2 py-2 max-sm:text-start md:text-xl  lg:text-2xl  font-extrabold">
+                  {project.title}
+                </p>
+                <p className=" px-2 py-1 max-sm:text-start text-xs md:text-sm xl:text-base  overflow-clip">
+                  {project.description}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+
+        {/* <div className="h-[300px] lg:h-[500px] flex lg:col-span-2 flex-col items-center justify-start bg-rich-black hover:bg-oil ">
           <div className="w-full">
-            <div className="h-[150px] md:h-[200px] lg:h-[300px] bg-[url('/assets/images/drug_allergy_app_image.svg')] bg-no-repeat bg-cover"></div>
+            <div className="h-[150px] md:h-[200px] lg:h-[300px] bg-[url('/assets/images/untitled_dragon_game.gif')] bg-no-repeat bg-cover"></div>
             <p className=" p-5  max-sm:text-start md:text-xl  lg:text-2xl  font-extrabold">
               Drug Allergy App
             </p>
           </div>
         </div>
-        {/* proj */}
         <div className="h-[300px] lg:h-[500px] flex lg:col-span-3 flex-col items-center justify-start bg-rich-black ">
           <div className="w-full">
             <div className="h-[150px] md:h-[200px] lg:h-[300px] bg-[url('/assets/images/open_hack_image.svg')] bg-no-repeat bg-cover"></div>
@@ -33,7 +54,7 @@ export default function ProjectSection(props: projectProps) {
               Birmingham Open Hack
             </p>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,15 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  safelist: [
+    "bg-[url('/assets/images/automated_pressure_test_image.svg')]",
+    "bg-[url('/assets/images/drug_allergy_app_image.svg')]",
+    "bg-[url('/assets/images/untitled_dragon_game.gif')]",
+    "bg-[url('/assets/images/open_hack_image.svg')]",
+    "col-span-3",
+    "col-span-2",
+    "col-span-5"
+  ],
   theme: {
     extend: {
       backgroundImage: {
@@ -14,16 +23,15 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        'rich-blue': '#111928',
-        'vanilla': '#EEE1B3',
-        'beige' : '#F5F5DC',
-        'rich-black' : '#0C121D',
-        'oil' : '#06090E',
-        'electric-indigo' : '#6320EE',
-        'flame': '#EB5E28'
+        "rich-blue": "#111928",
+        vanilla: "#EEE1B3",
+        beige: "#F5F5DC",
+        "rich-black": "#0C121D",
+        oil: "#06090E",
+        "electric-indigo": "#6320EE",
+        flame: "#EB5E28",
       },
     },
-
   },
   plugins: [],
 };
