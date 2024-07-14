4a250df37ba23c493f85157dff92a9536f29e1f9
Sid <siddharthjoshi1@hotmail.com>
2024-07-14 15:50:27 +0100

    Added conditional rendering for header components

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import { usePathname, useSearchParams } from "next/navigation";
 
 function scrollIntoView(
   mouseEvent:any,
@@ -11,6 +12,8 @@ function scrollIntoView(
 }
 
 export default function Header() {
+const pathname = usePathname();
+
   return (
     <div className=" flex flex-col lg:flex-row  items-center  justify-between md:pl-2 md:pr-8 2xl:px-24 overflow-hidden">
       <div>
@@ -23,35 +26,47 @@ export default function Header() {
           />
         </a>
       </div>
-      <div className=" w-1/2 xl:w-1/3 flex  flex-col lg:flex-row items-center justify-between">
-        <a
-        onClick={(e) => {
-           scrollIntoView(e, "projects-section");
-        }}
-          href="#projects-section"
-          className=" text-xl p-3 font-bold hover:text-beige hover:cursor-pointer "
-        >
-          Projects
-        </a>
-        <a
-          href="#experience-section"
-          onClick={(e) => {
-           scrollIntoView(e, "experience-section");
-          }}
-        >
-          <p className=" text-xl p-3 font-bold hover:text-beige hover:cursor-pointer ">
+      {pathname == "/" ? (
+        <div className=" w-1/2 xl:w-1/3 flex  flex-col lg:flex-row items-center justify-between">
+          <a
+            onClick={(e) => {
+              scrollIntoView(e, "projects-section");
+            }}
+            href="#projects-section"
+            className=" text-xl p-3 font-bold hover:text-beige hover:cursor-pointer "
+          >
+            Projects
+          </a>
+          <a
+            href="#experience-section"
+            onClick={(e) => {
+              scrollIntoView(e, "experience-section");
+            }}
+            className=" text-xl p-3 font-bold hover:text-beige hover:cursor-pointer "
+          >
             Experience
-          </p>
-        </a>
-        <button
-          className=" bg-oil hover:bg-gray-800  hover:cursor-pointer p-3 rounded-md "
-          onClick={(e) =>
-            (window.location.href = "mailto:contact@builtbysid.dev")
-          }
-        >
-          <p className="text-xl font-bold">Contact Me</p>
-        </button>
-      </div>
+          </a>
+          <button
+            className=" bg-oil hover:bg-gray-800  hover:cursor-pointer p-3 rounded-md "
+            onClick={(e) =>
+              (window.location.href = "mailto:contact@builtbysid.dev")
+            }
+          >
+            <p className="text-xl font-bold">Contact Me</p>
+          </button>
+        </div>
+      ) : (
+        <div className=" flex  flex-col lg:flex-row items-center justify-between">
+          <button
+            className=" bg-oil hover:bg-gray-800  hover:cursor-pointer p-3 rounded-md "
+            onClick={(e) =>
+              (window.location.href = "mailto:contact@builtbysid.dev")
+            }
+          >
+            <p className="text-xl font-bold">Contact Me</p>
+          </button>
+        </div>
+      )}
     </div>
   );
 }
